99c1695a044ab5c61b206b4b11386493b28bd411
enrico-favale <enrico.favale@edu.unife.it>
2024-12-23 01:22:56 +0100

    results.js

---
+++ assets/scripts/results.js
@@ -0,0 +1,13 @@
+const homepageButton = document.getElementById('homepage-button');
+
+homepageButton.addEventListener('click', () => {
+    localStorage.clear();
+    window.location.href = 'index.html';
+});
+
+const resultsScore = document.getElementById('results-score');
+const resultsTotal = document.getElementById('results-total');
+
+resultsScore.innerHTML = localStorage.getItem('correctAnswers');
+resultsTotal.innerHTML = localStorage.getItem('nQuestions');
+
