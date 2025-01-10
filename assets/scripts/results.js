07062cb93cf535092acf89a8ad70a8bfc78736a0
enrico-favale <enrico.favale@edu.unife.it>
2025-01-10 14:50:14 +0100

    Miglioramento pagina risultati

--- assets/scripts/results.js
+++ assets/scripts/results.js
@@ -8,6 +8,29 @@ homepageButton.addEventListener('click', () => {
 const resultsScore = document.getElementById('results-score');
 const resultsTotal = document.getElementById('results-total');
 
-resultsScore.innerHTML = localStorage.getItem('correctAnswers');
-resultsTotal.innerHTML = localStorage.getItem('nQuestions');
+const msgComplimenti = document.getElementById('msg-complimenti');
+const msgSuperamento = document.getElementById('msg-superamento');
 
+const coriandoliSX = document.getElementsByClassName('coriandoli-sx');
+const coriandoliDX = document.getElementsByClassName('coriandoli-dx');
+
+let correct_answers = localStorage.getItem('correctAnswers');
+let n_questions = localStorage.getItem('nQuestions');
+
+resultsScore.innerHTML = correct_answers;
+resultsTotal.innerHTML = n_questions;
+
+if (correct_answers >= n_questions / 2) {
+    msgComplimenti.innerHTML = 'Complimenti!';
+    msgSuperamento.innerHTML = 'Test superato';
+} else {
+    msgComplimenti.innerHTML = 'Ops...';
+    msgSuperamento.innerHTML = 'Test non superato';
+
+    for (let coriandolo of coriandoliSX) {
+        coriandolo.style.display = 'none';
+    }
+    for (let coriandolo of coriandoliDX) {
+        coriandolo.style.display = 'none';
+    }
+}
